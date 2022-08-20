cb76ac28f0a9c3e2f7453e9346526e0d100b6d8a
Jake McKown <jbmckown@gmail.com>
2022-08-19 18:16:42 -0700

    Fix build script

--- build.js
+++ build.js
@@ -15,27 +15,26 @@ import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const ROOT_DIR = 'app-icons';
 const GENERATED_FILE_HEADER = '/* THIS IS A GENERATED FILE; DO NOT MODIFY MANUALLY */\n';
-
-// Generate './icons/index.js'
 
 let contents = GENERATED_FILE_HEADER;
 
 const icons = {};
 
-readdirSync(resolve(__dirname, 'app-icons'), { withFileTypes: true })
+readdirSync(resolve(__dirname, ROOT_DIR), { withFileTypes: true })
   .filter(item => item.isDirectory())
 
   .forEach(directory => {
     const style = directory.name;
     const names = {};
 
-    readdirSync(resolve(__dirname, 'icons', style), { withFileTypes: true })
+    readdirSync(resolve(__dirname, ROOT_DIR, style), { withFileTypes: true })
       .filter(item => item.isFile() && extname(item.name) === '.svg')
 
       .forEach(file => {
         const name = basename(file.name, '.svg');
-        const svg = readFileSync(resolve(__dirname, 'icons', style, file.name), 'utf8')
+        const svg = readFileSync(resolve(__dirname, ROOT_DIR, style, file.name), 'utf8')
           .trim()
           .replace(/(?:\r?\n|\r)\s*/gm, '');
         
@@ -47,4 +46,4 @@ readdirSync(resolve(__dirname, 'app-icons'), { withFileTypes: true })
 
 contents += 'export const icons = ' + JSON.stringify(icons, null, 2) + ';';
 
-writeFileSync(resolve(__dirname, 'icons', 'index.js'), contents, 'utf8');
+writeFileSync(resolve(__dirname, ROOT_DIR, 'index.js'), contents, 'utf8');
