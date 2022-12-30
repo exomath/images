da3ad6e6e8bcd6fbf0149a7d38b42812844ffaa4
Jake McKown <jbmckown@gmail.com>
2022-12-30 16:49:38 -0700

    Update build to include logos

--- build.js
+++ build.js
@@ -1,40 +1,31 @@
-import {
-  readdirSync,
-  readFileSync,
-  writeFileSync
-} from 'fs';
-
-import {
-  basename,
-  dirname,
-  extname,
-  resolve
-} from 'path';
-
+import { readdirSync, readFileSync, writeFileSync } from 'fs';
+import { basename, dirname, extname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const ROOT_DIR = 'icons/app';
 const GENERATED_FILE_HEADER = '/* THIS IS A GENERATED FILE; DO NOT MODIFY MANUALLY */\n';
 
+/* Icons */
+
+let rootDir = 'icons/app';
 let contents = GENERATED_FILE_HEADER;
 
 const icons = {};
 
-readdirSync(resolve(__dirname, ROOT_DIR), { withFileTypes: true })
+readdirSync(resolve(__dirname, rootDir), { withFileTypes: true })
   .filter(item => item.isDirectory())
 
   .forEach(directory => {
     const style = directory.name;
     const names = {};
 
-    readdirSync(resolve(__dirname, ROOT_DIR, style), { withFileTypes: true })
+    readdirSync(resolve(__dirname, rootDir, style), { withFileTypes: true })
       .filter(item => item.isFile() && extname(item.name) === '.svg')
 
       .forEach(file => {
         const name = basename(file.name, '.svg');
-        const svg = readFileSync(resolve(__dirname, ROOT_DIR, style, file.name), 'utf8')
+        const svg = readFileSync(resolve(__dirname, rootDir, style, file.name), 'utf8')
           .trim()
           .replace(/(?:\r?\n|\r)\s*/gm, '');
         
@@ -46,4 +37,27 @@ readdirSync(resolve(__dirname, ROOT_DIR), { withFileTypes: true })
 
 contents += 'export const icons = ' + JSON.stringify(icons, null, 2) + ';';
 
-writeFileSync(resolve(__dirname, ROOT_DIR, 'index.js'), contents, 'utf8');
+writeFileSync(resolve(__dirname, rootDir, 'index.js'), contents, 'utf8');
+
+/* Logos */
+
+rootDir = 'logos';
+contents = GENERATED_FILE_HEADER;
+
+const logos = {};
+
+readdirSync(resolve(__dirname, rootDir), { withFileTypes: true })
+  .filter(item => item.isFile() && extname(item.name) === '.svg')
+
+  .forEach(file => {
+    const name = basename(file.name, '.svg');
+    const svg = readFileSync(resolve(__dirname, rootDir, file.name), 'utf8')
+      .trim()
+      .replace(/(?:\r?\n|\r)\s*/gm, '');
+    
+    logos[name] = svg;
+  });
+
+contents += 'export const logos = ' + JSON.stringify(logos, null, 2) + ';';
+
+writeFileSync(resolve(__dirname, rootDir, 'index.js'), contents, 'utf8');
